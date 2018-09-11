2d82672bcf857432da57164cc373fb3b37ebb03e
GaoGaoTaiga <nmmarzano@users.noreply.github.com>
2018-09-11 00:21:26 -0300

    More code cleanup

--- PerlinNoise.py
+++ PerlinNoise.py
@@ -3,23 +3,29 @@ import os
 import random
 import math
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 400, 400
+SCREEN_WIDTH, SCREEN_HEIGHT = 512, 512
 MAX_AMPLITUDE = 255/2
+
+def getBlendModeFor(value):
+    if(value<0):
+        return pygame.BLEND_SUB
+    else:
+        return pygame.BLEND_ADD
+
 
 def addToScreen(screen, frequency, amplitude):
     for i in range(frequency):
         for j in range(frequency):
             #square = pygame.Surface((SCREEN_WIDTH/frequency, SCREEN_HEIGHT/frequency))
             value = math.ceil(random.randint(0,math.ceil(amplitude*2))-amplitude)
-            posRect = pygame.Rect(i*(SCREEN_WIDTH/frequency), j*(SCREEN_HEIGHT/frequency), SCREEN_WIDTH/frequency, SCREEN_HEIGHT/frequency)
-            if value >= 0:
-                #square.fill((value, value, value))
-                #screen.blit(square, (i*(SCREEN_WIDTH/frequency), j*(SCREEN_HEIGHT/frequency)), special_flags=pygame.BLEND_ADD)
-                screen.fill((value, value, value), rect=posRect, special_flags=pygame.BLEND_ADD)
-            else:
-                #square.fill((-value, -value, -value))
-                #screen.blit(square, (i*(SCREEN_WIDTH/frequency), j*(SCREEN_HEIGHT/frequency)), special_flags=pygame.BLEND_SUB)
-                screen.fill((-value, -value, -value), rect=posRect, special_flags=pygame.BLEND_SUB)
+            hOffset = (SCREEN_HEIGHT/frequency)
+            wOffset = (SCREEN_WIDTH/frequency)
+            posRect = pygame.Rect(i*wOffset, j*hOffset, wOffset, hOffset)
+            blendMode = getBlendModeFor(value)
+            value = math.fabs(value)
+            #square.fill((value, value, value))
+            #screen.blit(square, (i*wOffset, j*hOffset), special_flags=pygame.BLEND_ADD)
+            screen.fill((value, value, value), rect=posRect, special_flags=blendMode)
 
 
 def waitForInput():
@@ -28,24 +34,36 @@ def waitForInput():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+    
+
+def frequencyFor(i):
+    return 2**(i+1)
+
+
+def amplitudeFor(i):
+    return MAX_AMPLITUDE/(1.4**i)
+
+
+def perlinNoise():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    screen.fill((127, 127, 127))
+    i = 0
+    while frequencyFor(i) < SCREEN_WIDTH: # so we don't go below the actual screen resolution
+        print("Iteration {0}...".format(i))
+        addToScreen(screen, frequencyFor(i), amplitudeFor(i))
+        #displays screen
+        pygame.display.flip()
+        print("Done.")
+        i+=1
+
+    print("All done!")
 
 
 def main():
     os.environ["SDL_VIDEO_CENTERED"] = "1"
     pygame.init()
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    screen.fill((127, 127, 127))
-    i = 0
-    while 2**(i+1)<SCREEN_WIDTH: # so we don't go below the actual screen resolution
-        print("Iteration {0}...".format(i))
-        addToScreen(screen, 2**(i+1), MAX_AMPLITUDE/(1.3**i))
-        #displays screen
-        pygame.display.flip()
-        print("Done.")
-        i+=1
-    
+    perlinNoise()
     waitForInput()
     pygame.quit()
 
