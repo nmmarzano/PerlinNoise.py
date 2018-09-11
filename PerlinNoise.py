69c14ed941fd095cbfd85c53b6f48d6824592a64
GaoGaoTaiga <nmmarzano@users.noreply.github.com>
2018-09-11 14:10:12 -0300

    A lot was done but everything works now

--- PerlinNoise.py
+++ PerlinNoise.py
@@ -3,26 +3,26 @@ import os
 import random
 import math
 import numpy
+from PIL import Image
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 512, 512
-MAX_AMPLITUDE = 255/2
+MAX_AMPLITUDE = 255*2/5
+MASTER_SEED = random.uniform(0, 1);
 
-MASTER_SEED = random.randint(100, 200);
 
-
-def deterministicRandom(x, y):
-    random.seed(x+MASTER_SEED)
+def deterministicRandom(x, y, extra):
+    random.seed(x+extra+MASTER_SEED)
+    random.seed(y*random.uniform(1,2)+extra+MASTER_SEED)
     result = random.uniform(-1, 1)
-    for i in range(y+4):
-        result = random.uniform(-1, 1)
     return result
 
 
-def smoothDeterministicRandom(x, y):
-    corners = deterministicRandom(x-1,y-1) + deterministicRandom(x+1,y-1) + deterministicRandom(x-1,y+1) + deterministicRandom(x+1,y+1)
-    sides = deterministicRandom(x-1,y) + deterministicRandom(x,y-1) + deterministicRandom(x+1,y) + deterministicRandom(x,y+1)
+# takes way too long and results are extremely bland, not recommended unless smoother noise is needed
+def smoothDeterministicRandom(x, y, extra):
+    corners = deterministicRandom(x-1,y-1,extra) + deterministicRandom(x+1,y-1,extra) + deterministicRandom(x-1,y+1,extra) + deterministicRandom(x+1,y+1,extra)
+    sides = deterministicRandom(x-1,y,extra) + deterministicRandom(x,y-1,extra) + deterministicRandom(x+1,y,extra) + deterministicRandom(x,y+1,extra)
 
-    return corners/16 + sides/8 + deterministicRandom(x, y)/4
+    return corners/16 + sides/8 + deterministicRandom(x, y,extra)/4
 
 
 def getBlendModeFor(value):
@@ -40,42 +40,16 @@ def waitForInput():
                 done = True
 
 
+# quickest but kinda harsh, not extremely bad though
 def lerp(a, b, x):
     return a*(1-x) + b*x
 
 
+# good smoothness and not too slow
 def cosineInterpolation(a, b, x):
     ft = x*math.pi
     f = (1 - math.cos(ft))*0.5
     return a*(1-f) + b*f
-   
-
-def randomGridAndInterpolateDraw(screen, frequency, amplitude, randomizer, interpolator):
-    hOffset = math.floor(SCREEN_HEIGHT/frequency)
-    wOffset = math.floor(SCREEN_WIDTH/frequency)
-    for x in range(SCREEN_WIDTH):
-        for y in range(SCREEN_HEIGHT):
-            prevX = math.floor(x/wOffset)
-            nextX = (math.floor(x/wOffset)+1)
-            prevY = math.floor(y/hOffset)
-            nextY = (math.floor(y/hOffset)+1)
-            fractionX = (x%wOffset)/wOffset
-            fractionY = (y%hOffset)/hOffset
-
-            v1 = randomizer(prevX,prevY)*amplitude
-            v2 = randomizer(nextX,prevY)*amplitude
-            v3 = randomizer(prevX,nextY)*amplitude
-            v4 = randomizer(nextX,nextY)*amplitude
-
-            i1 = interpolator(v1, v2, fractionX)
-            i2 = interpolator(v3, v4, fractionX)
-
-            value = interpolator(i1, i2, fractionY)
-            
-            blendMode = getBlendModeFor(value)
-            if value<0:
-                value = -value
-            screen.fill((value, value, value), rect=(x,y,1,1), special_flags=blendMode)
 
 
 def pixelArrayAndInterpolateDraw(screen, frequency, amplitude, randomizer, interpolator):
@@ -91,10 +65,10 @@ def pixelArrayAndInterpolateDraw(screen, frequency, amplitude, randomizer, inter
             fractionX = (x%wOffset)/wOffset
             fractionY = (y%hOffset)/hOffset
 
-            v1 = randomizer(prevX,prevY)*amplitude
-            v2 = randomizer(nextX,prevY)*amplitude
-            v3 = randomizer(prevX,nextY)*amplitude
-            v4 = randomizer(nextX,nextY)*amplitude
+            v1 = randomizer(prevX,prevY,frequency)*amplitude
+            v2 = randomizer(nextX,prevY,frequency)*amplitude
+            v3 = randomizer(prevX,nextY,frequency)*amplitude
+            v4 = randomizer(nextX,nextY,frequency)*amplitude
 
             i1 = interpolator(v1, v2, fractionX)
             i2 = interpolator(v3, v4, fractionX)
@@ -108,6 +82,39 @@ def pixelArrayAndInterpolateDraw(screen, frequency, amplitude, randomizer, inter
                 screenArray[x][y] = (255,255,255)
             else:
                 screenArray[x][y] = (aux+value, aux+value, aux+value)
+
+
+def pixelArrayAndInterpolateImg(screen, frequency, amplitude, randomizer, interpolator):
+    hOffset = math.floor(SCREEN_HEIGHT/frequency)
+    wOffset = math.floor(SCREEN_WIDTH/frequency)
+    for x in range(SCREEN_WIDTH):
+        for y in range(SCREEN_HEIGHT):
+            prevX = math.floor(x/wOffset)
+            nextX = (math.floor(x/wOffset)+1)
+            prevY = math.floor(y/hOffset)
+            nextY = (math.floor(y/hOffset)+1)
+            fractionX = (x%wOffset)/wOffset
+            fractionY = (y%hOffset)/hOffset
+
+            v1 = randomizer(prevX,prevY,frequency)*amplitude
+            v2 = randomizer(nextX,prevY,frequency)*amplitude
+            v3 = randomizer(prevX,nextY,frequency)*amplitude
+            v4 = randomizer(nextX,nextY,frequency)*amplitude
+
+            i1 = interpolator(v1, v2, fractionX)
+            i2 = interpolator(v3, v4, fractionX)
+
+            value = interpolator(i1, i2, fractionY)
+            
+            [aux, aux, aux] = screen[x,y]
+            if aux+value < 0:
+                screen[x,y] = [0,0,0]
+            elif aux+value > 255:
+                screen[x,y] = [255,255,255]
+            else:
+                screen[x,y] = [aux+value, aux+value, aux+value]
+
+    return screen
 
 
 def frequencyFor(i):
@@ -136,8 +143,24 @@ def perlinNoise():
     print("All done!")
 
 
+def imgPerlinNoise():
+    # Create a 1024x1024x3 array of 8 bit unsigned integers
+    screen = numpy.full( (SCREEN_WIDTH,SCREEN_HEIGHT,3), 127,dtype=numpy.uint8 )
+
+    i=0
+    while frequencyFor(i) < SCREEN_WIDTH:
+        print("Iteration {0}...".format(i))
+        screen = pixelArrayAndInterpolateImg(screen, frequencyFor(i), amplitudeFor(i), deterministicRandom, cosineInterpolation)
+        print("Done.")
+        i+=1
+
+    img = Image.fromarray(screen, mode='RGB')       # Create a PIL image
+    img.save('perlinnoise.png')                      # View in default viewer
+
+
 def main():
     os.environ["SDL_VIDEO_CENTERED"] = "1"
+    
     pygame.init()
     perlinNoise()
     waitForInput()
